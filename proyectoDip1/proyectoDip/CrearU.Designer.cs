5fe4b9649789012fc55e44b23a66d5d662e64515
DiegoBran16 <31348574+DiegoBran16@users.noreply.github.com>
2017-10-07 03:04:06 -0600

    Add files via upload

---
+++ proyectoDip1/proyectoDip/CrearU.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace proyectoDip
+{
+    partial class CrearU
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtGrupo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCargo = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lab = new System.Windows.Forms.Label();
+            this.txtContra = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(119, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(245, 62);
+            this.txtId.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 22);
+            this.txtId.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(119, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "grupo";
+            // 
+            // txtGrupo
+            // 
+            this.txtGrupo.Location = new System.Drawing.Point(245, 123);
+            this.txtGrupo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtGrupo.Name = "txtGrupo";
+            this.txtGrupo.Size = new System.Drawing.Size(100, 22);
+            this.txtGrupo.TabIndex = 3;
+            this.txtGrupo.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(119, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Cargo";
+            // 
+            // txtCargo
+            // 
+            this.txtCargo.Location = new System.Drawing.Point(245, 206);
+            this.txtCargo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtCargo.Name = "txtCargo";
+            this.txtCargo.Size = new System.Drawing.Size(100, 22);
+            this.txtCargo.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(391, 274);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 37);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "crear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lab
+            // 
+            this.lab.AutoSize = true;
+            this.lab.Location = new System.Drawing.Point(117, 263);
+            this.lab.Name = "lab";
+            this.lab.Size = new System.Drawing.Size(79, 17);
+            this.lab.TabIndex = 7;
+            this.lab.Text = "contraseña";
+            // 
+            // txtContra
+            // 
+            this.txtContra.Location = new System.Drawing.Point(245, 263);
+            this.txtContra.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtContra.Name = "txtContra";
+            this.txtContra.PasswordChar = '*';
+            this.txtContra.Size = new System.Drawing.Size(100, 22);
+            this.txtContra.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 288);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "regresar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // CrearU
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 341);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtContra);
+            this.Controls.Add(this.lab);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtCargo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtGrupo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "CrearU";
+            this.Text = "CrearU";
+            this.Load += new System.EventHandler(this.CrearU_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtGrupo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCargo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lab;
+        private System.Windows.Forms.TextBox txtContra;
+        private System.Windows.Forms.Button button2;
+    }
+}
